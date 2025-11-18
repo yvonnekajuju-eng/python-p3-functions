e7532e9f460e505e8aa0f447b88e46aeb8b6fa6d
yvvonne <yvonnekajuju6@gmail.com>
2025-11-18 12:10:32 +0300

    initial commit

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,18 @@
-#!/usr/bin/env python3
-
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
-def add(num1, num2):
-    pass
+
+def add(a, b):
+    return a + b
+
 
 def halve(number):
-    pass
+    return number / 2
